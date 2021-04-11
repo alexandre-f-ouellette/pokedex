0f4929134d150acc610077118e0cb7dab7e5e091
Alexandre Ouellette <alexandre.f.ouellette@gmail.com>
2021-04-11 15:30:07 -0400

    add uuids as default model ids

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_11_191153) do
+ActiveRecord::Schema.define(version: 2021_04_11_192810) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "pokemons", force: :cascade do |t|
