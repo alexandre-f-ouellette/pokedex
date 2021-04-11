62de4f6271a1e4d9379d8b3219fd1314622e5233
Alexandre Ouellette <alexandre.f.ouellette@gmail.com>
2021-04-11 16:18:06 -0400

    Add seeding script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+file = File.expand_path('./data/pokemon.csv', __dir__)
+
+SmarterCSV.process(file).each do |pokemon_data|
+  type1_string = pokemon_data.delete(:type_1)
+  type2_string = pokemon_data.delete(:type_2)
+
+  pokemon_data[:legendary] = pokemon_data[:legendary].downcase == 'true'
+
+  pokemon = Pokemon.new(pokemon_data.except(:total))
+
+  unless type1_string.nil?
+    type1 = Type.where(name: type1_string).first_or_initialize
+    pokemon.types << type1
+  end
+
+  unless type2_string.nil?
+    type2 = Type.where(name: type2_string).first_or_initialize
+    pokemon.types << type2
+  end
+
+  pokemon.save
+end
