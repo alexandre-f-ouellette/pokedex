36501b857a3dc6a78b0abfec29e57f50a2eb21f0
Alexandre Ouellette <alexandre.f.ouellette@gmail.com>
2021-04-11 15:48:00 -0400

    baseline type model

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: types
+#
+#  id         :uuid             not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Type < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_11_193510) do
+ActiveRecord::Schema.define(version: 2021_04_11_193637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2021_04_11_193510) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["deleted_at"], name: "index_pokemons_on_deleted_at"
+  end
+
+  create_table "types", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
